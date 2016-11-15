e6ad94126d102890ce55683b8d1450087f7ee3c8
Brendan Koral <brendanhkoral@gmail.com>
2016-11-15 16:31:27 -0600

    Set up main object and set its prototype

--- greetr.js
+++ greetr.js
@@ -1,3 +1,24 @@
 (function (global, $) {
 	
+	var Greetr = function(firstName, lastName, language) {
+		return new Greetr.init(firstName, lastName, language);
+	}
+
+	Greetr.prototype = {};
+
+	Greetr.init = function(firstName, lastName, language) {
+
+		//Set some defaults
+
+		//Capture 'this' at this stage to avoid issues with that keyword
+		var self = this;
+
+		self.firstName = firstName || '';
+		self.lastName = lastName || '';
+		self.language = language || 'en';
+
+	}
+
+	Greetr.init.prototype;
+
 }(window, jQuery));
